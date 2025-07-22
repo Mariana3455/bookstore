faf8573f3399e7e2d6cd9b8b81332eea3a27e84c
Mariana3455 <147875148+Mariana3455@users.noreply.github.com>
2025-07-22 18:58:21 +0300

    Update firebase.config.js

--- bookstoreClient/src/firebase/firebase.config.js
+++ bookstoreClient/src/firebase/firebase.config.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDV-m1r2-R7YJDTtZjrmCOfo3LReHMWzdw",
     authDomain: "fullstackbookstore-f55c3.firebaseapp.com",
@@ -13,6 +9,5 @@ const firebaseConfig = {
     appId: "1:704374954491:web:e8a8fe9f11b2b5708d135e"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export  default app;
